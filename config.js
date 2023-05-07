d96e59ce7b7ec32b02f0fc86cb240123efae2ad2
David Dernovoj <david@dernovoj.com>
2023-05-07 20:14:10 +0200

    Add public paths to the config

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     port: 3001,
     tokenTTL: 60 * 10 * 1000, // 10 minutes; 60 seconds * 10 minutes * 1000 milliseconds
+    publicPaths: [
+        { method: 'POST', path: '/api/session' },
+        { method: 'GET', path: '/login.html' },
+    ],
 };
