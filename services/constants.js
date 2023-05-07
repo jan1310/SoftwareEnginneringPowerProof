7bf242445c1fc7d7bbcdc3546ea31bdb0f960c73
David Dernovoj <david@dernovoj.com>
2023-05-07 21:48:22 +0200

    Add constants

---
+++ services/constants.js
@@ -0,0 +1,3 @@
+exports.USER_NOT_FOUND = 'Could not find the user';
+exports.SESSION_DOES_NOT_EXIST = 'The session already expired.';
+exports.NO_SESSION = 'The session has expired, please log in.';
